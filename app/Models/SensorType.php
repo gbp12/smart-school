7034d470bfa180daf026b4af475a5a1d4c2daf14
gbp12 <gonzabonilla4896@outlook.es>
2024-02-09 12:10:29 +0000

    hotfix

--- app/Models/SensorType.php
+++ app/Models/SensorType.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+require_once 'Sensor.php';
+
 class SensorType extends Model
 {
     use HasFactory;
