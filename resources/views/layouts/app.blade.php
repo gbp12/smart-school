ad5b48227cb5fec7933196d11ca465538a3d63df
gbp12 <gonzabonilla4896@outlook.es>
2024-02-15 11:43:56 +0000

    main structure created

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <title>@yield('title', 'Tienda online')</title>
+</head>
+
+<body>
+
+    <header> este es el header de la app </header>
+    @yield('content')
+    <footer> este es el footer de la app</footer>
+
+</body>
+
+</html>
