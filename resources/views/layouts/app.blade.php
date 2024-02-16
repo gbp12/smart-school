cfa3a2c7a869b0c90dfffe3153e20451a4135e6c
Nicolas <nicolascasclew@gmail.com>
2024-02-16 12:33:55 +0000

    marqueee

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
     <title>Smart School!</title>
 </head>
 
@@ -12,8 +13,33 @@
 
     <header> este es el header de la app </header>
     @yield('content')
-    <footer> este es el footer de la app</footer>
+    <div class="marquee">
+        <div class="track">
+            <div id=scrollText class="content">&nbsp;Test</div>
+        </div>
+    </div>
 
 </body>
+<script>
+let consejos = [
+    "Consejo 1: Lorem ipsum much texdo dolor tenet arepo opera rotas este es un cosejo poromocionado por el cabildo de gran canaria y financiado con vuestros impuestos jiji jaja money money me encanta el dinero",
+    "Consejo 2: Lorem ipsum much texdo dolor tenet arepo opera rotas este es un cosejo poromocionado por el cabildo de gran canaria y financiado con vuestros impuestos jiji jaja money money me encanta el dinero",
+    "Consejo 3: Lorem ipsum much texdo dolor tenet arepo opera rotas este es un cosejo poromocionado por el cabildo de gran canaria y financiado con vuestros impuestos jiji jaja money money me encanta el dinero",
+    "Consejo 4: Lorem ipsum much texdo dolor tenet arepo opera rotas este es un cosejo poromocionado por el cabildo de gran canaria y financiado con vuestros impuestos jiji jaja money money me encanta el dinero",
+]
+changeText();
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function changeText() {
+    let scroll = document.getElementById("scrollText")
+    //num = randomInteger(0, 3)
+    //newText = consejos[num]
+    //console.log(newText + " y num = ", num);
+    scroll.innerHTML = consejos[randomInteger(0, 3)]
+}
+</script>
 
 </html>
